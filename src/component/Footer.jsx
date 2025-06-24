07f4709c1b778b82b05e8a8fca9bfffc568316aa
kusham123 <preetibeniwal999@gmail.com>
2025-06-24 06:37:34 +0530

    improve gui

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-      <h2>Footer</h2>
+      
+      <section id="home" class=" flex items-center justify-center bg-gradient-to-r from-black  text-white text-center">
+    <div>
+      <h1 class="text-5xl font-bold mb-4">Footer</h1>
+      <p class="text-xl mb-6">Front-End Developer & Designer</p>
+      
+    </div>
+</section>
+
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-       <h2>Home</h2>
-       <h2>Home</h2>
-       <h2>Home</h2>
-       <h2>Home</h2>
-       <h2>Home</h2>
-    </div>
+    
+    <section id="home" class="h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-indigo-600 text-white text-center">
+      <div>
+        <h1 class="text-5xl font-bold mb-4">Hi, I'm Kusham Lata</h1>
+        <p class="text-xl mb-6">Front-End Developer & Designer</p>
+        <a href="#projects" class="bg-white text-blue-600 px-6 py-3 rounded-full font-semibold hover:bg-gray-200">View Projects</a>
+      </div>
+    </section>
+
   )
 }
 
